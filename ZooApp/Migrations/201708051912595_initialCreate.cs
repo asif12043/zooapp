a7e356c14150a76b870c4db4870e1eb221adbd78
asif rahman <oronno.mbstu.cse@outlook.com>
2017-08-06 02:05:52 +0600

    zooapp first migrations

---
+++ ZooApp/Migrations/201708051912595_initialCreate.cs
@@ -0,0 +1,29 @@
+namespace ZooApp.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class initialCreate : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Animals",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(),
+                        Food = c.String(),
+                        Origin = c.String(),
+                        Quantiy = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Animals");
+        }
+    }
+}
---
+++ ZooApp/Models/Animal.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ZooApp.Models
+{
+    public class Animal
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Food { get; set; }
+
+        public string Origin { get; set; }
+
+        public int Quantiy { get; set; }
+    }
+
+}
